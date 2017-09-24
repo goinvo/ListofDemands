fb175fc26b9d52e9854241fcc6b2057540843b5f
Rob Sterner <rob@rescuedcode.com>
2017-09-24 17:43:01 -0400

    remove bogus delivery_method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,6 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :mailgun
-
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
