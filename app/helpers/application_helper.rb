d265c79e0272003f4cd6eaf52cdf1d48f0c600e8
Rob Sterner <rob@rescuedcode.com>
2017-12-01 21:27:58 -0500

    make the change area go to profiles for logged in people

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def edit_area_or_profile_path
+    if user_signed_in?
+      edit_profile_path
+    else
+      edit_area_path(set_area_redirect: request.original_url)
+    end
+  end
 end
