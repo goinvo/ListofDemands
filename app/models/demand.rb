a2d765286300c19e661c0d5670b2e7c42a6ecec5
Rob Sterner <rob@rescuedcode.com>
2017-12-20 06:41:59 -0500

    automatically demand created demands by the creator

--- app/models/demand.rb
+++ app/models/demand.rb
@@ -8,7 +8,17 @@ class Demand < ApplicationRecord
   delegate :name, to: :problem
   attr_accessor :problem_text
 
+  before_create :create_user_demand
+
   def problem_text=(name)
     self.problem = Problem.find_or_create_by(name: name)
+  end
+
+  private
+
+  def create_user_demand
+    if user_demands.blank?
+      user_demands.build(user: user)
+    end
   end
 end
