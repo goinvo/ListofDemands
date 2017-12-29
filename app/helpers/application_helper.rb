7d3ac71f96154722e874793c67c11699d7dd08c6
Rob Sterner <rob@rescuedcode.com>
2017-12-29 11:10:13 -0500

    add user’s area to the brand area of the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
       edit_area_path(set_area_redirect: request.original_url)
     end
   end
+
+  def user_header
+    if content_for(:brand)
+      yield(:brand)
+    else
+      user_signed_in? ?
+        current_user.area.name :
+        ""
+    end
+  end
 end
