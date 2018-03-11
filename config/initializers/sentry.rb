30b78a0607ad306725ac840474ae77a152a27ed8
Rob Sterner <fermion@github.com>
2018-03-11 14:05:42 -0400

    disable sentry for now

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,9 @@
-if ENV['SENTRY_DSN'].present?
-
-  Raven::Configuration::IGNORE_DEFAULT = []
-  # puts "Raven::Configuration::IGNORE_DEFAULT: #{Raven::Configuration::IGNORE_DEFAULT}"
-
-  Raven.configure do |config|
-    config.dsn = ENV['SENTRY_DSN']
-  end
-end
+# if ENV['SENTRY_DSN'].present?
+#
+#   Raven::Configuration::IGNORE_DEFAULT = []
+#   # puts "Raven::Configuration::IGNORE_DEFAULT: #{Raven::Configuration::IGNORE_DEFAULT}"
+#
+#   Raven.configure do |config|
+#     config.dsn = ENV['SENTRY_DSN']
+#   end
+# end
