82b9014892426a2fe93f138ad020ef188e4a469e
craigmcginley <craigmcginley@gmail.com>
2018-05-17 20:31:25 -0400

    add ability to unsupport a demand

--- spec/controllers/supported_demands_controller_spec.rb
+++ spec/controllers/supported_demands_controller_spec.rb
@@ -44,4 +44,51 @@ RSpec.describe SupportedDemandsController, type: :controller do
       expect(response).to redirect_to(@demand_to_support)
     end
   end
+
+  describe "#destroy" do
+    before do
+      @demand_to_remove = @experience.demands[:boxford_user_boxford_1]
+      @user.user_demands.create(demand: @demand_to_remove)
+    end
+
+    it "deletes a user_demand for a municipal-level demand" do
+      expect(@user.user_demands.length).to be(1)
+      delete :destroy, params: { id: @demand_to_remove.id }
+      @user.user_demands.reload
+
+      expect(@user.user_demands).to be_empty
+    end
+
+    it "deletes a user_demand for a state-level demand" do
+      @demand_to_remove = @experience.demands[:boxford_user_mass]
+      @user.user_demands.create(demand: @demand_to_remove)
+      expect(@user.user_demands.length).to be(2)
+      delete :destroy, params: { id: @demand_to_remove.id }
+      @user.user_demands.reload
+
+      expect(@user.user_demands.length).to be(1)
+    end
+
+    it "doesn't delete a demand if the requested demand to delete is non-existent" do
+      @demand_to_remove = @experience.demands[:boxford_user_mass]
+      expect(@user.user_demands.length).to be(1)
+      delete :destroy, params: { id: @demand_to_remove.id }
+      @user.user_demands.reload
+
+      expect(@user.user_demands.length).to be(1)
+    end
+
+    it "redirects to correct url when given 'redirect' param" do
+      url = 'foo.bar/baz'
+      delete :destroy, params: { id: @demand_to_remove.id, redirect: url }
+
+      expect(response).to redirect_to(url)
+    end
+
+    it "redirects to the newly supported demand when no 'redirect' param is given" do
+      delete :destroy, params: { id: @demand_to_remove.id }
+
+      expect(response).to redirect_to(@demand_to_remove)
+    end
+  end
 end
