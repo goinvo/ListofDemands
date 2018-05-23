d0cb7d891aa9fa2a9ebdf7e3b7785f7208bd1c7d
Rob Sterner <fermion@github.com>
2018-05-22 21:29:31 -0400

    WIP

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -7,11 +7,37 @@ RSpec.describe SearchController, type: :controller do
   end
 
   describe "GET show" do
-    it "assigns @demands" do
-      get :show
+    context "searching locally" do
+      before do
+        stub_arlington_geoip
+      end
 
-      expect(response.status).to eq(200)
-      expect(assigns(:demands)).to eq([@experience.demands[:arlington_user_arlington]])
+      it "returns demands for Arlington" do
+        get :show
+
+        expect(response.status).to eq(200)
+        expect(assigns(:demands)).to eq([@experience.demands[:arlington_user_arlington]])
+      end
+    end
+
+    context "searching in the county" do
+      it "returns demands for Middlesex" do
+        get :show, params: { scope: 'county' }
+
+        expect(response.status).to eq(200)
+        expect(assigns(:demands)).to match_array(
+          County.find_by(name: "Middlesex, Massachusetts").demands
+        )
+      end
+    end
+
+    context "searching in the state" do
+      it "returns demands for Massachusetts" do
+        get :show, params: { scope: 'state' }
+
+        expect(response.status).to eq(200)
+        expect(assigns(:demands)).to eq([@experience.demands[:arlington_user_arlington]])
+      end
     end
   end
 end
---
+++ spec/lib/demand_search_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DemandSearch do
+  before do
+  end
+
+  # describe "GET show" do
+  #   it "assigns @demands" do
+  #     build_demand(area: @area, user: @user)
+  #
+  #     get :show
+  #
+  #     expect(response.status).to eq(200)
+  #     expect(assigns(:demands)).to eq(Demand.all.to_a)
+  #   end
+  # end
+
+  def build_demand(area:, user:, count: 1)
+    # count.times do
+    #   create(:demand, area: area, user: user)
+    # end
+  end
+end
