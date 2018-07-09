f0a728c68b010c3a47a80f83e8d72e96fbf272c1
craigmcginley <craigmcginley@gmail.com>
2018-07-09 12:31:55 -0400

    force utf-8 encoding on usernames

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,8 +5,16 @@ class Profile < ApplicationRecord
   validates :zip, :name, presence: true
   validates :username, uniqueness: true, format: { with: /\A(?=.*[a-z])[a-z\d\-\_]+\Z/i, message: "Only letters, numbers, hyphens, and underscores are allowed. Must contain at least one letter." }
 
-  # TODO: Still need to validate username is utf-8
-  # https://stackoverflow.com/questions/8635578/how-to-check-whether-the-character-is-utf-8
+  before_create :encode_username
+  before_update :encode_username
+
+  def encode_username
+    username.encode(Encoding.find("UTF-8"), {invalid: :replace, undef: :replace, replace: ""})
+  end
+
+  # TODO: Test username is forced to utf-8 encoding
+  # https://stackoverflow.com/questions/12947910/force-strings-to-utf-8-from-any-encoding
+  # http://graysoftinc.com/character-encodings/ruby-19s-string
 
   # TODO: Might want to ensure username is not an existing UUID (and vice versa?)
   # https://stackoverflow.com/questions/10049047/rails-validate-uniqueness-based-on-another-table
