2b91d42cd3cd01422459196d13765be6b9e1a3ca
craigmcginley <craigmcginley@gmail.com>
2020-06-15 15:28:54 -0400

    add JSON response option for users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
   before_action :check_user_privacy, only: [:show]
-  before_action :ensure_user_municipality, only: [:show]
+  before_action :ensure_user_municipality, only: [:show], unless: :format_json?
 
   def show
     @user ||= UserView.new(find_user)
+    demands_json = []
+
+    if format_json?
+      @user.wrapped_supported_demands.each do |demand|
+        demand_attrs = demand.attributes
+        demand_attrs["demand_description"] = demand.demand_description
+        demands_json << demand_attrs
+      end
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: demands_json }
+    end
   end
 
   private
@@ -18,5 +32,9 @@ class UsersController < ApplicationController
     if !@user || @user.private?
       raise ActionController::RoutingError.new('Not Found')
     end
+  end
+
+  def format_json?
+    request.format.symbol == :json
   end
 end
