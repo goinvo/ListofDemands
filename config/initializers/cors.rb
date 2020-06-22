eaf40a494f774b9c21d4030a707ebfb3dc21eb3e
craigmcginley <craigmcginley@gmail.com>
2020-06-22 14:56:55 -0400

    allow all origins for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8000', 'https://www.goinvo.com'
+    origins '*'
     resource '*', headers: :any, methods: [:get]
   end
 end
